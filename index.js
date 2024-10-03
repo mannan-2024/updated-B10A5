7e0b9e9d4f5c669f605a37a3a56bd373c8cf60c6
mannan-2024 <shaikh.mannan.1978@gmail.com>
2024-10-03 13:32:32 +0600

    adding all cards

--- index.js
+++ index.js
@@ -7,21 +7,29 @@
                                                                 })
         
         document.getElementById('donate-button').addEventListener('click',function(){
+                
                 const donateMoney = document.getElementById('input-amount-field').value ;
                 const donateNumber = parseFloat(donateMoney);
                 const balance = document.getElementById('balance-amount').innerText ;
                 const balanceNumber = parseFloat(balance);
                 const accountBalance = document.getElementById('account-balance').innerText;
                 const accountBalanceNumber = parseFloat(accountBalance);
+                
                 if (!isNaN(donateMoney) === true && donateNumber > 0 ) {
-                                       
+                                                       
                 const newBalance = donateNumber + balanceNumber;
                 document.getElementById('balance-amount').innerText = newBalance;
+                
                 const newAccountBalance = accountBalanceNumber - donateNumber;
                 document.getElementById('account-balance').innerText = newAccountBalance;
+                
                 const entry = document.createElement('p');
                 entry.innerText = `Tk.${donateMoney} is donated for Donate for flood in Noakhali,Bangladesh`;
                 document.getElementById('Transaction-history').appendChild(entry);
+                document.getElementById('my-modal').open = true;
+                const d = document.createElement('p');
+                d.innerText = Date();
+                document.getElementById('Transaction-history').appendChild(d);
                 }
                 else{
                         alert('invalid donation amount');
@@ -31,6 +39,9 @@
                           
 
                 
+        })
+        document.getElementById('close-modal').addEventListener('click',function(){
+                document.getElementById('my-modal').open = false;
         })
        
         document.getElementById('history-button').addEventListener('click',function(){
